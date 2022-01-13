3b3434c370ea01270356ce2fe60b6389b30a184d
VRRemi <63209429+VRRemi@users.noreply.github.com>
2022-01-12 17:56:33 -0800

    Created config.js
    Gotta find a place to make the importo

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    token: "", // Discord Bot Token obtained from discord.com/developers
+    prefix: "o!", // Prefix responded for the bot
+    owners: ["ID", "ID"] // Users who can use Owner Commands ex: ["ID"] or ["ID", "ID", "ID"]
+}
